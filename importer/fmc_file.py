c1b2945dea814b6ce254c5eafc371d31c7ef10f7
AlphaSheep <brendan@alphasheep.co.za>
2022-11-18 20:25:57 +0200

    Add importer for manual FMC results

---
+++ importer/fmc_file.py
@@ -0,0 +1,33 @@
+import csv
+from datetime import timedelta, datetime
+
+from importer.base_timer_importer import ITimerImporter
+from result import Result
+
+FMC_RESULT_FILES = [
+    '../CSVDumps/FMCResults.csv']
+
+FMC_CATEGORIES = ['3x3 Fewest Moves'] # Correspond to the files in FMC_RESULT_FILES
+
+
+class FMCFileImporter(ITimerImporter):
+
+    def import_all(self) -> None:
+        for iFile in range(len(FMC_RESULT_FILES)):
+            source_file_name = FMC_RESULT_FILES[iFile]
+
+            category = FMC_CATEGORIES[iFile].strip()
+            self.categories.add(category)
+
+            source = 'Manual FMC Results: ' + source_file_name
+
+            with open(source_file_name) as file_stream:
+                csv_file = csv.reader(file_stream)
+                for solution in csv_file:
+                    start = datetime.strptime(solution[0], '%Y-%m-%d')
+                    time = timedelta(seconds=float(solution[1]))
+                    penalty = timedelta(seconds=0)
+
+                    result = Result(start, time, category, penalty, source)
+                    self.results.append(result)
+
