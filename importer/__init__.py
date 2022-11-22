70b0ae8c3715588e7156ca671b82d7047d3411de
AlphaSheep <brendan@alphasheep.co.za>
2022-11-22 21:08:36 +0200

    Add a schema for validating importer config

--- importer/__init__.py
+++ importer/__init__.py
@@ -1,5 +1,6 @@
 import json
 from typing import List, TypeVar, Union, Set, Dict, Any
+from jsonschema import validate
 
 from result import Result
 from .base_timer_importer import ITimerImporter
@@ -12,7 +13,9 @@ from .prisma import PrismaImporter
 
 Importer = TypeVar('Importer', bound=ITimerImporter)
 
-DEFAULT_CONFIG_FILE = "../importconfig.json"
+
+DEFAULT_CONFIG_FILE = "import_config.json"
+CONFIG_SCHEMA_FILE = "importer/config_schema.json"
 
 
 def get_importer(name: str) -> Importer:
@@ -29,6 +32,12 @@ def get_importer(name: str) -> Importer:
             return FMCFileImporter()
 
 
+def validate_config(config: Dict[str, Any]):
+    with open(CONFIG_SCHEMA_FILE) as file_stream:
+        schema = json.load(file_stream)
+    validate(instance=config, schema=schema)
+
+
 class ImportEngine:
     def __init__(self):
         self._importers: List[Importer] = []
@@ -36,13 +45,15 @@ class ImportEngine:
     def load_configurations(self, config_file: str = DEFAULT_CONFIG_FILE) -> None:
         with open(config_file) as file_stream:
             config = json.load(file_stream)
+        validate_config(config)
+
         if "imports" in config.keys():
             importers = config["imports"]
             for importer in importers:
                 self._configure_and_attach_importer(importer)
 
     def _configure_and_attach_importer(self, importer_config: Dict[str, Any]) -> None:
-        importer: Importer = get_importer(importer_config["name"])
+        importer: Importer = get_importer(importer_config["engine"])
 
         match importer_config["type"]:
             case "latest":
