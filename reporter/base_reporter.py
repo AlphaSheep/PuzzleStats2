5aae763272beca3afe8a979cd50233056251ae29
AlphaSheep <brendan@alphasheep.co.za>
2023-09-20 21:47:01 +0200

    Add an abstract BaseReporter

---
+++ reporter/base_reporter.py
@@ -0,0 +1,19 @@
+
+from abc import ABC, abstractmethod
+
+from analysis import AnalysisEngine
+
+class BaseReporter(ABC):
+
+    def __init__(self):
+        self.analysis: AnalysisEngine
+
+        self.output_file: str = ''
+        self.output_folder: str = ''
+
+    def attach_analysis(self, analysis: AnalysisEngine):
+        self.analysis = analysis
+
+    @abstractmethod
+    def generate(self):
+        raise NotImplementedError
