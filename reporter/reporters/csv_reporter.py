35f22ec71a01c007c5409387fac4876348ee1313
AlphaSheep <brendan@alphasheep.co.za>
2023-10-07 12:14:37 +0200

    Make CSV reporter report all as a chunk instead of per category

--- reporter/reporters/csv_reporter.py
+++ reporter/reporters/csv_reporter.py
@@ -1,18 +1,12 @@
-
-from pandas import DataFrame, concat
-
 from ..base_reporter import BaseReporter
 
 class CSVReporter(BaseReporter):
+
     def __init__(self) -> None:
+        super().__init__()
         self.output_file: str = ''
 
-    def generate(self):
-
-        df: DataFrame = DataFrame()
-
-        for category in self.analysis.get_categories():
-            df = concat([df, self.analysis.get_dataframe(category)], axis=0)
-
+    def generate(self) -> None:
+        df = self.analysis.get_dataframe()
         with open(self.output_file, 'w', newline='') as file_stream:
-            df.to_csv(file_stream, index=True, header=True, index_label='timestamp')
+            df.to_csv(file_stream, index=True, header=True)
